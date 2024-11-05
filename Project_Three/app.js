67ba930acc4bc2f6154a7dc335ab59bc72151945
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-11-04 20:13:44 -0600

    event listener

---
+++ Project_Three/app.js
@@ -0,0 +1,7 @@
+const circle = document.querySelector('#circle')
+
+circle.addEventListener('mouseenter', () => {
+    if(!circle.classList.contains('hover')) {
+        circle.classList.add('hover');
+    }
+}) ;
