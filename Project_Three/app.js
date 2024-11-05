1810cb1f01721d649c28312d79f154ee8cd7939e
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-11-04 20:17:05 -0600

    event listener added

--- Project_Three/app.js
+++ Project_Three/app.js
@@ -5,3 +5,9 @@ circle.addEventListener('mouseenter', () => {
         circle.classList.add('hover');
     }
 }) ;
+
+circle.addEventListener('mouseleave', () => {
+    if(circle.classList.contains('hover')) {
+        circle.classList.remove('hover');
+    }
+}) ;
